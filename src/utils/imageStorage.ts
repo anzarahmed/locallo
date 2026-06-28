0f0f1e49e8c20f8cec015f997732c3dc930f52e5
anzarahmed <anzar822@gmail.com>
2026-06-28 20:02:40 +0530

    commented  access and secret key

--- src/utils/imageStorage.ts
+++ src/utils/imageStorage.ts
@@ -5,10 +5,10 @@ import path from 'path';
 
 const s3 = new S3Client({
   region: process.env.AWS_REGION!,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
+  // credentials: {
+  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  // },
 });
 
 const BUCKET = process.env.AWS_S3_BUCKET!;
