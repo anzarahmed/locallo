609f8b3e897eb7541af953f354315026fc0145ff
anzarahmed <anzar822@gmail.com>
2026-08-02 19:11:46 +0530

    fix issue

--- src/controllers/customer/cmsPageController.ts
+++ src/controllers/customer/cmsPageController.ts
@@ -4,7 +4,7 @@ import { getPublicCmsPage } from '../../services/customer/cmsPageService';
 
 export async function getCmsPage(req: Request, res: Response): Promise<void> {
   try {
-    const page = await getPublicCmsPage(req.params.slug);
+    const page = await getPublicCmsPage(String(req.params.slug));
     sendSuccess(res, { cmsPage: page }, 'Page fetched');
   } catch (err: unknown) {
     handleServiceError(err, res, 'Failed to load page');
