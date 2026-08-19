5d684336f50fa8178d06856f7df01f68fda3d98b
anzarahmed <anzar822@gmail.com>
2026-08-19 21:43:30 +0530

    update database config for production

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,16 +1,26 @@
 require('dotenv').config();
 
+const dialectOptions = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+};
+
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions,
   },
   test: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions,
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions,
   },
 };
