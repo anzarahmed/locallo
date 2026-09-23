b9bf7436272d5653400ea41f15a0bec7d942f66c
anzarahmed <anzar822@gmail.com>
2026-09-23 22:38:29 +0530

    cms updated to manage both for seller and customer

--- src/services/customer/cmsPageService.ts
+++
@@ -1,9 +0,0 @@
-import { CmsPage } from '../../models/CmsPage';
-
-export async function getPublicCmsPage(slug: string): Promise<CmsPage> {
-  const page = await CmsPage.findOne({ where: { slug, isActive: true } });
-  if (!page) {
-    throw Object.assign(new Error('Page not found'), { status: 404 });
-  }
-  return page;
-}
